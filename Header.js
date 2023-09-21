e29c9af876416b2da69680c9389e2fd7b7880f6a
Aditya Shukla <142572085+shuklaadity@users.noreply.github.com>
2023-09-21 18:28:21 +0530

    add new feature

---
+++ Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Header=()=>{
+
+    return(
+        <>
+         <div className="nav-div">
+           
+          <h2>Keep Note</h2>
+         </div>
+        
+        </>
+    )
+
+}
+export default Header;
---
+++ Nateapp.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "./Header";
+import "./Notapp.css";
+
+
+
+const Noteapp =()=>{
+  return(
+    <>
+     <Header/>
+     <h1> Note App</h1>
+     
+    </>
+  )
+
+}
+
+export default Noteapp;
